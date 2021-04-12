a51fc1d2ee96b2aceaa53933c3ad94558868544f
palexdev <alessandro.parisi406@gmail.com>
2021-04-12 13:02:18 +0200

    Fixed JLink demo
    Signed-off-by: palexdev <alessandro.parisi406@gmail.com>

--- demo/src/main/java/module-info.java
+++ demo/src/main/java/module-info.java
@@ -7,6 +7,7 @@ module MaterialFX.demo.main {
 
     requires fr.brouillard.oss.cssfx;
     requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.ikonli.fontawesome5;
     requires org.scenicview.scenicview;
 
     opens io.github.palexdev.materialfx.demo;
