8ff4754afa4801223e30ac133be4c96f2ca9b136
palexdev <alessandro.parisi406@gmail.com>
2021-06-03 13:19:06 +0200

    :bulb: Updated project copyright
    Signed-off-by: palexdev <alessandro.parisi406@gmail.com>

--- demo/src/main/java/io/github/palexdev/materialfx/demo/controllers/TogglesController.java
+++ demo/src/main/java/io/github/palexdev/materialfx/demo/controllers/TogglesController.java
@@ -1,3 +1,21 @@
+/*
+ *     Copyright (C) 2021 Parisi Alessandro
+ *     This file is part of MaterialFX (https://github.com/palexdev/MaterialFX).
+ *
+ *     MaterialFX is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     MaterialFX is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with MaterialFX.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package io.github.palexdev.materialfx.demo.controllers;
 
 import io.github.palexdev.materialfx.controls.MFXRectangleToggleNode;
