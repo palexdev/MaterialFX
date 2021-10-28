abcc2e6375758fa9737f0d7ffe889e1f779dea14
Tech-Expert-Wizard <77369194+Tech-Expert-Wizard@users.noreply.github.com>
2021-10-27 21:22:22 -0400

    Fix compiler error when building

--- materialfx/src/main/java/io/github/palexdev/materialfx/skins/MFXCheckboxSkin.java
+++ materialfx/src/main/java/io/github/palexdev/materialfx/skins/MFXCheckboxSkin.java
@@ -144,12 +144,10 @@ public class MFXCheckboxSkin extends SkinBase<MFXCheckbox> {
          * then the center of the ripple is set to the width and/or height of container
          */
         checkBox.addEventFilter(MouseEvent.MOUSE_CLICKED, event -> {
-            if (!event, checkBox)) {
-                return;
+            if (!NodeUtils.inHierarchy(event.getPickResult().getIntersectedNode(), checkBox)) {
+                rippleGenerator.generateRipple(event);
+                checkBox.fire();
             }
-
-            rippleGenerator.generateRipple(event);
-            checkBox.fire();
         });
 
         /*
