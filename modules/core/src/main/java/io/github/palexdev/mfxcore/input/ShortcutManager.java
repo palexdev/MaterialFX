8dd2d05c9feae12b8ed87123c1bff7c3950c97eb
palexdev <alessandro.parisi406@gmail.com>
2026-04-07 12:21:28 +0200

    :sparkles: (core) Implement ShortcutManager to make KeyStrokes handling automatic/less of a hassle

---
+++ modules/core/src/main/java/io/github/palexdev/mfxcore/input/ShortcutManager.java
@@ -0,0 +1,214 @@
+/*
+ * Copyright (C) 2026 Parisi Alessandro - alessandro.parisi406@gmail.com
+ * This file is part of MaterialFX (https://github.com/palexdev/MaterialFX)
+ *
+ * MaterialFX is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * MaterialFX is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with MaterialFX. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package io.github.palexdev.mfxcore.input;
+
+import java.lang.ref.Cleaner;
+import java.util.*;
+
+import io.github.palexdev.mfxcore.observables.When;
+import io.github.palexdev.mfxcore.popups.menu.MFXMenu;
+import io.github.palexdev.mfxcore.popups.menu.MFXMenuItem;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+
+/// A manager for keyboard shortcuts at the [Scene] level. Tries to mimic what JavaFX does with
+/// `ControlAcceleratorSupport`, but in a more generic and transparent way, integrating with [KeyStroke] and [KeyMap].
+///
+/// Shortcuts are managed through [Action] objects which pair a [KeyStroke] with a [Runnable]. A [KeyMap] stores the
+/// shortcuts, intercepts events on the scene, and runs the corresponding actions when the shortcut is matched.
+///
+/// #### Installation Modes
+///
+/// 1) [#installOn(Scene, Action...)] - Installs shortcuts directly on a scene.<br >
+/// This is the recommended approach when you have a reference to the scene.
+///
+/// 2) [#installOn(Node, Action...)] - Installs shortcuts on a node's scene.<br >
+/// When the node's scene changes, shortcuts are automatically migrated.
+/// Shortcuts are disposed when the node is garbage collected (via [Cleaner]).
+///
+/// 3) [#installFor(MFXMenu)] - Convenience method to install shortcuts for all [MFXMenuItem]s that have a shortcut.<br >
+/// (the system is similar to JavaFX's menu items with the `accelerator` property)
+///
+/// **Usage Example**
+///
+/// ```java
+/// ShortcutManager.installOn(scene,
+///     Action.action("Ctrl+S", () -> save()),
+///     Action.action("Ctrl+Z", () -> undo())
+/// );
+/// ```
+public class ShortcutManager {
+
+    //================================================================================
+    // Properties
+    //================================================================================
+
+    private static final Map<Scene, ShortcutManager> sceneShortcutManagers = new WeakHashMap<>();
+    private static final Cleaner cleaner = Cleaner.create();
+
+    private final KeyMap keyMap = new KeyMap();
+
+    //================================================================================
+    // Constructors
+    //================================================================================
+
+    private ShortcutManager() {}
+
+    //================================================================================
+    // Methods
+    //================================================================================
+
+    /// Installs the given [Action]s on the specified [Scene].
+    ///
+    /// @see #doInstall(Scene, Action...)
+    public static void installOn(Scene scene, Action... actions) {
+        Objects.requireNonNull(scene);
+        doInstall(scene, actions);
+    }
+
+    /// Installs shortcuts for all [MFXMenuItems][MFXMenuItem] in the given [MFXMenu] that have a [shortcut][KeyStroke].
+    ///
+    /// This is a convenience method that extracts actions from menu items and calls [#installOn(Scene, Action...)]
+    /// on the menu's root owner scene. The [Runnable] for the [Action] simply calls [MFXMenuItem.MFXMenuItemBehavior#runAction()].
+    ///
+    /// @throws IllegalStateException if the menu is not installed yet
+    public static void installFor(MFXMenu menu) {
+        MFXMenu root = menu.getRootMenu();
+        if (!root.isInstalled()) throw new IllegalStateException("Menu is not installed yet, therefore owner is null!");
+        Action[] actions = menu.getItems().stream()
+            .filter(it -> it.getShortcut() != null)
+            .map(it -> new Action(
+                it.getShortcut(),
+                () -> ((MFXMenuItem.MFXMenuItemBehavior) it.getBehavior()).runAction())
+            )
+            .toArray(Action[]::new);
+        installOn(root.getOwner(), actions);
+    }
+
+    /// Removes shortcuts for all [MFXMenuItem]s in the given [MFXMenu] that have a shortcut.<br >
+    /// Basically the inverse of [#installFor(MFXMenu)].
+    ///
+    /// @throws IllegalStateException if the menu is not installed yet
+    public static void uninstallFor(MFXMenu menu) {
+        MFXMenu root = menu.getRootMenu();
+        if (!root.isInstalled()) throw new IllegalStateException("Menu is not installed yet, therefore owner is null!");
+        Action[] actions = menu.getItems().stream()
+            .filter(it -> it.getShortcut() != null)
+            .map(it -> new Action(
+                it.getShortcut(),
+                null) // since we don's include the Runnable in equality checks, we don't need to instantiate them here
+            )
+            .toArray(Action[]::new);
+        uninstallFrom(root.getOwner(), actions);
+    }
+
+    /// Installs the given [Action]s on the scene of the specified [Node].
+    ///
+    /// Unlike [#installOn(Scene, Action...)], this method automatically handles scene changes.
+    /// When the node's scene changes, shortcuts are removed from the old scene and installed on the new one.
+    ///
+    /// Shortcuts are automatically disposed when the node is garbage collected.
+    public static void installOn(Node node, Action... actions) {
+        When<Scene> listener = When.onChanged(node.sceneProperty())
+            .then((o, n) -> {
+                if (o != null) doUninstall(o, actions);
+                if (n != null) doInstall(n, actions);
+            })
+            .executeNow(() -> node.getScene() != null)
+            .listen();
+        cleaner.register(node, listener::dispose);
+    }
+
+    /// Completely uninstalls the [ShortcutManager] for the given [Scene] (all actions are removed).
+    ///
+    /// @see #doUninstall(Scene, Action...)
+    public static void uninstallFrom(Scene scene) {
+        doUninstall(scene, (Action[]) null);
+    }
+
+    /// Removes the given [Action]s from the scene of the specified [Node].<br >
+    /// Basically the inverse of [#installOn(Node, Action...)].
+    public static void uninstallFrom(Node node, Action... actions) {
+        When<Scene> listener = When.onInvalidated(node.sceneProperty())
+            .condition(Objects::nonNull)
+            .then((s) -> doUninstall(s, actions))
+            .executeNow(() -> node.getScene() != null)
+            .oneShot(true)
+            .listen();
+        if (!listener.isDisposed())
+            cleaner.register(node, listener::dispose);
+    }
+
+    /// Creates or retrieves the [ShortcutManager] for the given scene, adds all actions to its [KeyMap],
+    /// and installs the [KeyMap] if not already installed.
+    private static void doInstall(Scene scene, Action... actions) {
+        ShortcutManager manager = sceneShortcutManagers.computeIfAbsent(scene, _ -> new ShortcutManager());
+        for (Action action : actions) {
+            manager.keyMap.put(action.shortcut(), (_, _) -> action.action().run());
+        }
+        if (!manager.keyMap.isInstalled()) {
+            cleaner.register(scene, manager.keyMap::dispose);
+            manager.keyMap.install(scene, false);
+        }
+    }
+
+    /// Removes the given actions from the scene's [KeyMap].<br >
+    /// If the [KeyMap] becomes empty, it is disposed and the [ShortcutManager] is removed from the internal registry.
+    private static void doUninstall(Scene scene, Action... actions) {
+        Optional.ofNullable(sceneShortcutManagers.get(scene))
+            .ifPresent(sm -> {
+                if (actions == null) {
+                    sm.keyMap.dispose();
+                } else {
+                    Arrays.stream(actions).map(Action::shortcut).forEach(sm.keyMap::remove);
+                    if (sm.keyMap.isEmpty()) sm.keyMap.dispose();
+                }
+                sceneShortcutManagers.remove(scene);
+            });
+    }
+
+    //================================================================================
+    // Inner Classes
+    //================================================================================
+
+    /// A record that pairs a [KeyStroke] with a [Runnable] action, used by [ShortcutManager] to define keyboard shortcuts.
+    ///
+    /// _Note: the action is excluded from equality checks and hashCode_
+    public record Action(KeyStroke shortcut, Runnable action) {
+
+        public static Action action(KeyStroke shortcut, Runnable action) {return new Action(shortcut, action);}
+
+        /// Uses [KeyStroke#of(String)] to resolve from a string.
+        public static Action action(String shortcut, Runnable action) {
+            return action(KeyStroke.of(shortcut), action);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (o == null || getClass() != o.getClass()) return false;
+            Action action = (Action) o;
+            return Objects.equals(shortcut(), action.shortcut());
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hashCode(shortcut());
+        }
+    }
+}
---
+++ modules/core/src/test/java/interactive/ShortcutManagerTest.java
@@ -0,0 +1,451 @@
+/*
+ * Copyright (C) 2026 Parisi Alessandro - alessandro.parisi406@gmail.com
+ * This file is part of MaterialFX (https://github.com/palexdev/MaterialFX)
+ *
+ * MaterialFX is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * MaterialFX is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with MaterialFX. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package interactive;
+
+import java.lang.ref.WeakReference;
+import java.lang.reflect.Field;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import io.github.palexdev.mfxcore.input.ShortcutManager;
+import javafx.event.Event;
+import javafx.event.EventTarget;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.api.FxRobot;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
+
+import static io.github.palexdev.mfxcore.input.ShortcutManager.Action.action;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
+@ExtendWith(ApplicationExtension.class)
+public class ShortcutManagerTest {
+
+    private Stage stage;
+    private Scene scene;
+    private StackPane root;
+
+    @Start
+    void start(Stage stage) {
+        this.stage = stage;
+        root = new StackPane();
+        scene = new Scene(root, 400, 300);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        sceneManagers().clear();
+    }
+
+    //================================================================================
+    // Install(scene)
+    //================================================================================
+
+    @Test
+    @DisplayName("installOn(Scene): null scene throws NPE")
+    void installOnScene_nullThrows() {
+        assertThrows(NullPointerException.class,
+            () -> ShortcutManager.installOn((Scene) null, action("A", () -> {})));
+    }
+
+    @Test
+    @DisplayName("installOn(Scene): fires on matching key")
+    void installOnScene_fires() {
+        AtomicInteger c = new AtomicInteger();
+        ShortcutManager.installOn(scene, action("A", c::incrementAndGet));
+        fire(scene, KeyCode.A);
+        assertEquals(1, c.get());
+    }
+
+    @Test
+    @DisplayName("installOn(Scene): fires every time the shortcut is pressed")
+    void installOnScene_firesRepeatedly() {
+        AtomicInteger c = new AtomicInteger();
+        ShortcutManager.installOn(scene, action("A", c::incrementAndGet));
+
+        fire(scene, KeyCode.A);
+        fire(scene, KeyCode.A);
+        fire(scene, KeyCode.A);
+        assertEquals(3, c.get());
+    }
+
+    @Test
+    @DisplayName("installOn(Scene): wrong key does not fire")
+    void installOnScene_wrongKey() {
+        AtomicInteger c = new AtomicInteger();
+        ShortcutManager.installOn(scene, action("A", c::incrementAndGet));
+
+        fire(scene, KeyCode.B);
+        assertEquals(0, c.get());
+    }
+
+    @Test
+    @DisplayName("installOn(Scene): missing modifier does not fire")
+    void installOnScene_missingModifier(FxRobot robot) {
+        AtomicInteger c = new AtomicInteger();
+        ShortcutManager.installOn(scene, action("Ctrl+S", c::incrementAndGet));
+
+        fire(scene, KeyCode.S);
+        assertEquals(0, c.get());
+    }
+
+    @Test
+    @DisplayName("installOn(Scene): correct modifier fires")
+    void installOnScene_correctModifier() {
+        AtomicInteger c = new AtomicInteger();
+        ShortcutManager.installOn(scene, action("Ctrl+S", c::incrementAndGet));
+
+        fire(scene, KeyCode.S, KeyCode.CONTROL);
+        assertEquals(1, c.get());
+    }
+
+    @Test
+    @DisplayName("installOn(Scene): extra modifier does not fire")
+    void installOnScene_extraModifier() {
+        AtomicInteger c = new AtomicInteger();
+        ShortcutManager.installOn(scene, action("Ctrl+S", c::incrementAndGet));
+
+        fire(scene, KeyCode.S, KeyCode.CONTROL, KeyCode.SHIFT);
+        assertEquals(0, c.get());
+    }
+
+    @Test
+    @DisplayName("installOn(Scene): multiple actions on different shortcuts fire independently")
+    void installOnScene_multipleActions() {
+        AtomicInteger cA = new AtomicInteger(), cB = new AtomicInteger();
+        ShortcutManager.installOn(scene,
+            action("A", cA::incrementAndGet),
+            action("B", cB::incrementAndGet));
+
+        fire(scene, KeyCode.A);
+        fire(scene, KeyCode.B);
+        assertEquals(1, cA.get());
+        assertEquals(1, cB.get());
+    }
+
+    @Test
+    @DisplayName("installOn(Scene): two calls on the same scene accumulate actions")
+    void installOnScene_accumulatesAcrossCalls() {
+        AtomicInteger cA = new AtomicInteger(), cB = new AtomicInteger();
+        ShortcutManager.installOn(scene, action("A", cA::incrementAndGet));
+        ShortcutManager.installOn(scene, action("B", cB::incrementAndGet));
+
+        fire(scene, KeyCode.A);
+        fire(scene, KeyCode.B);
+        assertEquals(1, cA.get());
+        assertEquals(1, cB.get());
+    }
+
+    @Test
+    @DisplayName("installOn(Scene): duplicate shortcut on same scene throws")
+    void installOnScene_duplicateThrows() {
+        ShortcutManager.installOn(scene, action("A", () -> {}));
+        assertThrows(Exception.class,
+            () -> ShortcutManager.installOn(scene, action("A", () -> {})));
+    }
+
+    //================================================================================
+    // Install(Node)
+    //================================================================================
+
+    @Test
+    @DisplayName("installOn(Node): fires when node is already in scene")
+    void installOnNode_alreadyInScene() {
+        AtomicInteger c = new AtomicInteger();
+        ShortcutManager.installOn(root, action("A", c::incrementAndGet));
+
+        fire(scene, KeyCode.A);
+        assertEquals(1, c.get());
+    }
+
+    @Test
+    @DisplayName("installOn(Node): does not fire when node has no scene")
+    void installOnNode_noScene() {
+        AtomicInteger c = new AtomicInteger();
+        ShortcutManager.installOn(new StackPane(), action("A", c::incrementAndGet));
+
+        fire(scene, KeyCode.A);
+        assertEquals(0, c.get());
+    }
+
+    @Test
+    @DisplayName("installOn(Node): registers when node is added to scene after installation")
+    void installOnNode_registersOnAdd(FxRobot robot) {
+        AtomicInteger c = new AtomicInteger();
+        StackPane child = new StackPane();
+        ShortcutManager.installOn(child, action("A", c::incrementAndGet));
+
+        robot.interact(() -> root.getChildren().add(child));
+        fire(scene, KeyCode.A);
+        assertEquals(1, c.get());
+    }
+
+    @Test
+    @DisplayName("installOn(Node): unregisters when node is removed from scene")
+    void installOnNode_unregistersOnRemoval(FxRobot robot) {
+        AtomicInteger c = new AtomicInteger();
+        StackPane child = new StackPane();
+        robot.interact(() -> root.getChildren().add(child));
+        ShortcutManager.installOn(child, action("A", c::incrementAndGet));
+
+        robot.interact(() -> root.getChildren().remove(child));
+        fire(scene, KeyCode.A);
+        assertEquals(0, c.get());
+    }
+
+    @Test
+    @DisplayName("installOn(Node): re-registers when node is re-added to scene")
+    void installOnNode_reRegistersOnReattach(FxRobot robot) {
+        AtomicInteger c = new AtomicInteger();
+        StackPane child = new StackPane();
+        robot.interact(() -> root.getChildren().add(child));
+        ShortcutManager.installOn(child, action("A", c::incrementAndGet));
+
+        robot.interact(() -> root.getChildren().remove(child));
+        robot.interact(() -> root.getChildren().add(child));
+        fire(scene, KeyCode.A);
+        assertEquals(1, c.get());
+    }
+
+    @Test
+    @DisplayName("installOn(Node): does not fire while node is between scenes")
+    void installOnNode_noFireWhileDetached(FxRobot robot) {
+        AtomicInteger c = new AtomicInteger();
+        StackPane child = new StackPane();
+        robot.interact(() -> root.getChildren().add(child));
+        ShortcutManager.installOn(child, action("A", c::incrementAndGet));
+
+        robot.interact(() -> root.getChildren().remove(child));
+        fire(scene, KeyCode.A);
+        assertEquals(0, c.get());
+    }
+
+    @Test
+    @DisplayName("installOn(Node): removal only affects node's own actions, not other scene-level shortcuts")
+    void installOnNode_onlyOwnActionsRemoved(FxRobot robot) {
+        AtomicInteger cA = new AtomicInteger(), cB = new AtomicInteger();
+        StackPane child = new StackPane();
+        robot.interact(() -> root.getChildren().add(child));
+        ShortcutManager.installOn(child, action("A", cA::incrementAndGet));
+        ShortcutManager.installOn(scene, action("B", cB::incrementAndGet));
+
+        robot.interact(() -> root.getChildren().remove(child));
+        fire(scene, KeyCode.A);
+        fire(scene, KeyCode.B);
+        assertEquals(0, cA.get(), "Node's shortcut should be gone");
+        assertEquals(1, cB.get(), "Scene-level shortcut should still fire");
+    }
+
+    @Test
+    @DisplayName("installOn(Node): action migrates to new scene when node is moved")
+    void installOnNode_migratesOnSceneChange(FxRobot robot) {
+        AtomicInteger c = new AtomicInteger();
+        StackPane child = new StackPane();
+        robot.interact(() -> root.getChildren().add(child));
+        Scene scene2 = new Scene(new StackPane(), 400, 300);
+        ShortcutManager.installOn(child, action("A", c::incrementAndGet));
+
+        robot.interact(() -> {
+            root.getChildren().remove(child);
+            ((StackPane) scene2.getRoot()).getChildren().add(child);
+        });
+
+        fire(scene, KeyCode.A);
+        assertEquals(0, c.get(), "Should not fire on old scene");
+
+        fire(scene2, KeyCode.A);
+        assertEquals(1, c.get(), "Should fire on new scene");
+    }
+
+    @Test
+    @DisplayName("installOn(Node): multiple nodes can own distinct shortcuts on the same scene")
+    void installOnNode_multipleNodesDistinctShortcuts(FxRobot robot) {
+        AtomicInteger cA = new AtomicInteger(), cB = new AtomicInteger();
+        StackPane c1 = new StackPane(), c2 = new StackPane();
+        robot.interact(() -> root.getChildren().addAll(c1, c2));
+        ShortcutManager.installOn(c1, action("A", cA::incrementAndGet));
+        ShortcutManager.installOn(c2, action("B", cB::incrementAndGet));
+
+        fire(scene, KeyCode.A);
+        fire(scene, KeyCode.B);
+        assertEquals(1, cA.get());
+        assertEquals(1, cB.get());
+    }
+
+    //================================================================================
+    // Install(MFXMenu)
+    //================================================================================
+
+    @Test
+    @DisplayName("installFor: throws when root menu is not installed")
+    void installFor_notInstalled() {
+
+    }
+
+    @Test
+    @DisplayName("installFor: items without shortcut are silently skipped")
+    void installFor_skipsNullShortcutItems() {
+
+    }
+
+    @Test
+    @DisplayName("installFor: shortcut items fire actions on the owner's scene")
+    void installFor_firesShortcutItems() {
+
+    }
+
+    @Test
+    @DisplayName("installFor: only items with shortcuts are registered")
+    void installFor_mixedItems() {
+
+    }
+
+
+    //================================================================================
+    // Uninstall
+    //================================================================================
+
+    @Test
+    @DisplayName("uninstall: all actions stop firing")
+    void uninstall_stopsActions() {
+        AtomicInteger c = new AtomicInteger();
+        ShortcutManager.installOn(scene, action("A", c::incrementAndGet));
+
+        ShortcutManager.uninstallFrom(scene);
+        fire(scene, KeyCode.A);
+        assertEquals(0, c.get());
+    }
+
+    @Test
+    @DisplayName("uninstall: null scene does not throw")
+    void uninstall_null() {
+        assertDoesNotThrow(() -> ShortcutManager.uninstallFrom(null));
+    }
+
+    @Test
+    @DisplayName("uninstall: unknown scene does not throw")
+    void uninstall_unknownScene() {
+        assertDoesNotThrow(() -> ShortcutManager.uninstallFrom(new Scene(new StackPane())));
+    }
+
+    @Test
+    @DisplayName("uninstall: can reinstall same shortcut afterwards")
+    void uninstall_thenReinstall() {
+        AtomicInteger c = new AtomicInteger();
+        ShortcutManager.installOn(scene, action("A", c::incrementAndGet));
+        ShortcutManager.uninstallFrom(scene);
+
+        ShortcutManager.installOn(scene, action("A", c::incrementAndGet));
+        fire(scene, KeyCode.A);
+        assertEquals(1, c.get());
+    }
+
+    //================================================================================
+    // Memory Management
+    //================================================================================
+
+    @Test
+    @DisplayName("Memory: scene entry is removed from WeakHashMap after GC")
+    void memory_sceneGCed() throws Exception {
+        Map<?, ?> map = sceneManagers();
+
+        WeakReference<Scene> ref;
+        Scene local = new Scene(new StackPane(), 100, 100);
+        ShortcutManager.installOn(local, action("A", () -> {}));
+        ref = new WeakReference<>(local);
+        assertEquals(1, map.size());
+
+        local = null;
+        awaitGC(ref);
+        assumeTrue(ref.get() == null, "GC did not collect the scene - skipping");
+
+        //noinspection ResultOfMethodCallIgnored
+        map.size(); // triggers WeakHashMap internal stale-entry expunge
+        assertEquals(0, map.size());
+    }
+
+    @Test
+    @DisplayName("Memory: node-installed shortcut does not prevent scene from being GC'd")
+    void memory_nodeInstallDoesNotRetainScene() throws Exception {
+        Map<?, ?> map = sceneManagers();
+        StackPane localRoot = new StackPane();
+
+        WeakReference<Scene> ref;
+        Scene local = new Scene(localRoot, 100, 100);
+        ShortcutManager.installOn(localRoot, action("A", () -> {}));
+        ref = new WeakReference<>(local);
+
+        local.setRoot(new Region());
+        local = null;
+        localRoot = null;
+        awaitGC(ref);
+        assumeTrue(ref.get() == null, "GC did not collect the scene - skipping");
+
+        //noinspection ResultOfMethodCallIgnored
+        map.size();
+        assertEquals(0, map.size());
+    }
+
+    //================================================================================
+    // Helpers
+    //================================================================================
+
+    private static void fire(EventTarget target, KeyCode code, KeyCode... modifiers) {
+        Set<KeyCode> mods = Set.of(modifiers);
+        KeyEvent e = new KeyEvent(
+            KeyEvent.KEY_PRESSED,
+            code.getName(), code.getName(),
+            code,
+            mods.contains(KeyCode.SHIFT),
+            mods.contains(KeyCode.CONTROL),
+            mods.contains(KeyCode.ALT),
+            mods.contains(KeyCode.META)
+        );
+        Event.fireEvent(target, e);
+    }
+
+    /// Returns the private static sceneShortcutManagers map via reflection.
+    private static Map<?, ?> sceneManagers() throws Exception {
+        Field f = ShortcutManager.class.getDeclaredField("sceneShortcutManagers");
+        f.setAccessible(true);
+        return (Map<?, ?>) f.get(null);
+    }
+
+    /// Hints GC in a short loop until the referent is collected or the attempt limit is reached.
+    /// GC is non-deterministic; callers must guard with assumeTrue(ref.get() == null).
+    private static void awaitGC(WeakReference<?> ref) throws InterruptedException {
+        for (int i = 0; i < 50 && ref.get() != null; i++) {
+            System.gc();
+            Thread.sleep(100);
+        }
+    }
+}
